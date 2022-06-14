d898ba7ce4ce9e2558eed387296d1fcb227ff8f0
w4bo <m.francia@unibo.it>
2022-06-14 21:20:51 +0200

    fixing the scripts

--- download.sh
+++ download.sh
@@ -5,6 +5,7 @@ if [ ! -f "instantclient-basic-linux.x64-21.1.0.0.0.zip" ]; then
     curl -k -o instantclient-basic-linux.x64-21.1.0.0.0.zip https://big.csr.unibo.it/projects/nosql-datasets/instantclient-basic-linux.x64-21.1.0.0.0.zip
     unzip instantclient-basic-linux.x64-21.1.0.0.0.zip
     chmod -R 777 instantclient_21_1
+    pwd
 fi
 ls -las
 cd -
