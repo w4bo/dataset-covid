df9c74205184c446ee3064693bf10733d9eef917
Matteo Francia <m.francia@unibo.it>
2022-06-15 15:47:17 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -10,7 +10,7 @@ if [ ! -f "instantclient-basic-linux.x64-21.1.0.0.0.zip" ]; then
 fi
 
 if [ ! -f "COOL-all.jar" ]; then 
-    curl -k -o COOL-all.jar -L https://github.com/big-unibo/conversational-olap/releases/download/1.0.1/COOL-all.jar
+    curl -k -o COOL-all.jar -L https://github.com/big-unibo/conversational-olap/releases/download/1.0.2/COOL-all.jar
 fi
 
 ls -las
