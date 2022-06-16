bb4c6ee7582052613743120ef52312095b673eb7
Matteo Francia <m.francia@unibo.it>
2022-06-16 14:01:20 +0200

    Feature deploy (#12)
    * add an alternative impdp, since importing data from/tu users requires additional parameters

--- dockerimages/oracle11g/f02_oracle_init.sh
+++ dockerimages/oracle11g/f02_oracle_init.sh
@@ -5,6 +5,7 @@ chmod -R 777 /data
 ls -las /data
 
 # impdp covid_weekly/oracle@127.0.0.1:1521/xe DIRECTORY=ORACLE_DUMP DUMPFILE=COVID_WEEKLY.DMP SCHEMAS=covid_weekly
+# impdp covid_weekly/oracle@127.0.0.1:1521/xe touser=covid_weekly_102 fromuser=covid_weekly DIRECTORY=ORACLE_DUMP DUMPFILE=COVID_WEEKLY.DMP SCHEMAS=covid_weekly
 
 touch /data/.ready
 
